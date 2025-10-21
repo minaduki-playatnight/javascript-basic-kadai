495c849fc42d443751a294efb3c172ba8a295b02
minaduki-playatnight <minase0402zzz@gmail.com>
2025-10-21 22:29:46 +0900

    MDN課題

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+
+const today = new Date();
+
+let Year = today.getFullYear(); 
+let Month = today.getMonth()+1;
+let Day = today.getDate();
+
+console.log( Year + '年' + Month + '月' + Day + '日');
+
+
+
+
